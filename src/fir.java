6a20e7656a1c0561ebfe954d52cc372fae8a6753
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-17 10:17:18 -0700

    ds

--- src/fir.java
+++ src/fir.java
@@ -1,5 +1,7 @@
 public class fir {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("dfdsf");
+        System.out.println("Rahul chutiye");
     }
 }
